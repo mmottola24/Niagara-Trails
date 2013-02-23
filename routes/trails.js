7e9bac8b98d5cbbfd80df1e486aafeeb29438d45
unknown <OpenDataDay@furi-dev0.windows.office.furi.ca>
2013-02-23 16:13:22 -0500

    Removed test-trails route, using real trails now

--- routes/trails.js
+++ routes/trails.js
@@ -1,11 +1,12 @@
 
 /*
- * Test Page
+ * List all the trails by name
  */
 
 exports.list = function(req, res){
   var Trails = require('../models/trails');
   Trails.fetch(function(err, result) {
+    res.contentType('application/json');
     if (err) res.send(err);
     else res.send(result);
   });
